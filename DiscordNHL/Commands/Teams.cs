ec689a8093ba84f6621c772b6791f604df06bf4c
Teemu Heino <teemu.heino@tuni.fi>
2021-06-13 12:43:41 +0300

    Add command for fetching rosters.

--- DiscordNHL/Commands/Teams.cs
+++ DiscordNHL/Commands/Teams.cs
@@ -1,5 +1,6 @@
 ﻿using Discord;
 using Discord.Commands;
+using DiscordNHL.Dtos.StatsAPI;
 using DiscordNHL.Extensions;
 using DiscordNHL.Integrations;
 using DiscordNHL.Services;
@@ -18,39 +19,99 @@ namespace DiscordNHL.Commands
             _provider = provider;
         }
         [Command("")]
-        public async Task GetTeamBySearchString(string search)
+        public async Task GetTeamByAbbreviation(string abbreviation)
         {
             try
             {
-                var response = await _provider.GetTeams();
+                var id = await GetTeamIdByAbbreviation(abbreviation);
 
-                if (response.IsSuccess)
-                {
-                    var team = response.Data.Teams.FirstOrDefault(it => 
-                    string.Equals(it.Abbreviation, search, StringComparison.InvariantCultureIgnoreCase) 
-                    || 
-                    search.Length >= 5 && it.Name.Contains(search, StringComparison.InvariantCultureIgnoreCase)
-                    );
+                var response = await _provider.GetTeamById(id);
+
+                if (response.IsSuccess) {
+                    var team = response.Data.Teams.FirstOrDefault();
 
                     if (team != null)
                     {
 
                         var embed = new EmbedBuilder()
                             .AddGeneralFields(CommandHandler.Discord)
-                            .AddNHLDataFields(team.ToEmbedData())
+                            .AddNHLDataFields(team.ToTeamEmbedData())
                             .Build();
 
                         await Context.Channel.SendMessageAsync(null, false, embed);
                     }
                     else
                     {
-                        await Context.Channel.SendMessageAsync($"Team with search {search} not found");
+                        await Context.Channel.SendMessageAsync($"Team with search {abbreviation} not found");
                     }
                 }
             }
             catch
             {
                 await Context.Channel.SendMessageAsync($"An error occured");
+            }
+        }
+
+        [Command("roster")]
+        public async Task GetTeamRosterByAbbreviation(string abbreviation)
+        {
+            try
+            {
+                var id = await GetTeamIdByAbbreviation(abbreviation);
+
+                var response = await _provider.GetFullTeamById(id);
+
+                if (response.IsSuccess)
+                {
+                    var team = response.Data.Teams.FirstOrDefault();
+
+                    if (team != null)
+                    {
+
+                        var embed = new EmbedBuilder()
+                            .AddGeneralFields(CommandHandler.Discord)
+                            .AddNHLDataFields(team.ToRosterEmbedData())
+                            .Build();
+
+                        await Context.Channel.SendMessageAsync(null, false, embed);
+                    }
+                    else
+                    {
+                        await Context.Channel.SendMessageAsync($"Team roster with search {abbreviation} not found");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await Context.Channel.SendMessageAsync($"An error occured");
+            }
+        }
+
+        private async Task<int> GetTeamIdByAbbreviation(string abbreviation) {
+            var abbrev = abbreviation.ToUpper();
+
+            if (StaticDataService.TeamIdByAbbreviation == null)
+            {
+                var response = await _provider.GetTeams();
+
+                if (response.IsSuccess)
+                {
+                    StaticDataService.SetTeamIds(response.Data);
+                } 
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+
+
+            if (StaticDataService.TeamIdByAbbreviation.TryGetValue(abbrev, out int teamId))
+            {
+                return teamId;
+            }
+            else
+            {
+                return -1;
             }
         }
     }
--- DiscordNHL/Extensions/Mappings.cs
+++ DiscordNHL/Extensions/Mappings.cs
@@ -1,12 +1,13 @@
 ﻿using DiscordNHL.Dtos.StatsAPI;
 using DiscordNHL.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DiscordNHL.Extensions
 {
     public static class Mappings
     {
-        public static EmbedData ToEmbedData(this TeamDto team)
+        public static EmbedData ToTeamEmbedData(this TeamDto team)
         {
             var embedData = new EmbedData
             {
@@ -27,6 +28,55 @@ namespace DiscordNHL.Extensions
             };
 
             return embedData;
+        }
+
+        public static EmbedData ToRosterEmbedData(this TeamDto team) 
+        {
+            var embedData = new EmbedData
+            {
+                Title = team.Name,
+                Description = $"Roster of the {team.TeamName}",
+                Url = team.OfficialSiteUrl
+            };
+
+            embedData.Data = new List<EmbedValue>();
+
+            if (team.Roster != null) {
+                var roster = team.Roster.Roster;
+
+                var playerTypes = roster?
+                    .Where(it => it.Person?.FullName != null && it.Position?.Name != null && it.Position?.Type != null)
+                    .OrderBy(it => it.Person.FullName)
+                    .ThenBy(it => it.Position.Name)
+                    .GroupBy(it => it.Position.Type);
+
+                var goalies = playerTypes?.FirstOrDefault(it => it.Key == "Goalie");
+                var defense = playerTypes?.FirstOrDefault(it => it.Key == "Defenseman");
+                var forwards = playerTypes?.FirstOrDefault(it => it.Key == "Forward");
+
+                var goalieNames = string.Join("\n", goalies?.Select(it => string.Join(", ", it.Person.FullName, GetJerseyNumberString(it.JerseyNumber))));
+                embedData.Data.Add(new EmbedValue("Goalies", goalieNames));
+                var defenseNames = string.Join("\n", defense?.Select(it => string.Join(", ", it.Person.FullName,  GetJerseyNumberString(it.JerseyNumber))));
+                embedData.Data.Add(new EmbedValue("Defensemen", defenseNames));
+                var forwardNames = string.Join("\n", forwards?.Select(it => string.Join(", ", it.Person.FullName, it.Position.Name, GetJerseyNumberString(it.JerseyNumber))));
+                embedData.Data.Add(new EmbedValue("Forwards", forwardNames));
+            }
+
+            if (embedData.Data.Count == 0) 
+            {
+                embedData.Description = "No active roster players found";
+            }
+
+            return embedData;
+        }
+
+        private static string GetJerseyNumberString(string jerseyNumber) {
+            if(jerseyNumber != null)
+            {
+                return $"#{jerseyNumber}";
+            }
+
+            return null;
         }
     }
 }
